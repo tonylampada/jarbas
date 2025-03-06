295964720929a40d4d8d21a74c9e6a01d4fca6b6
tonylampada <tonylampada@gmail.com>
2025-03-06 09:10:18 -0300

    skeleton

---
+++ jarbas/configs.py
@@ -0,0 +1,18 @@
+def init():
+    pass
+
+def get_mcp_servers():
+    pass
+
+def get_agents():
+    pass
+
+def get_default_model():
+    pass
+
+def set_default_agent(agent_name):
+    pass
+
+def set_default_model(model_name):
+    pass
+
